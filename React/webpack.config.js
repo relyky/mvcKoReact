f67e87d1f91516c047348aace0c25471cd083ee5
Rely Ky <relyky@hotmail.com>
2019-10-17 22:08:43 +0800

    init webpack+babel+react configuration

---
+++ React/webpack.config.js
@@ -0,0 +1,13 @@
+﻿module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    }
+};
