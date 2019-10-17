f2ec7313361309c67123b715f87e88c50934a645
Rely Ky <relyky@hotmail.com>
2019-10-17 20:31:25 +0800

    step

---
+++ mvcKoReact/Areas/KoLab/Controllers/KoBindReactController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcKoReact.Areas.KoLab.Controllers
+{
+    public class KoBindReactController : Controller
+    {
+        // GET: KoLab/KoBindReact
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
