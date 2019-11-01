ff4bb882a5df9c27506b43a94c87cc3ea80e9d03
Rely Ky <relyky@hotmail.com>
2019-11-02 02:00:52 +0800

    pivot

--- React/src/index.js
+++ React/src/index.js
@@ -12,20 +12,19 @@ function App() {
             FormContainer: lazy(() => import('./js/components/container/FormContainer.jsx')),
             FormA01: lazy(() => import('./js/AppForm/FormA01/AppForm.jsx')),
             FormB02: lazy(() => import('./js/AppForm/FormB02/AppForm.jsx')),
-            FormC03: lazy(() => import('./js/AppForm/FormC03/AppForm.jsx'))
+            FormC03: lazy(() => import('./js/AppForm/FormC03/AppForm.jsx')),
+            FormD04: lazy(() => import('./js/AppForm/FormD04/AppForm.jsx'))
         }
         setAppFormPool(appFormPool)
     }, []) // <--- 此空[]將使useEffect只執行一次。等同componentDidMount
 
-
     // 取得顯示目標元件
     const AppForm = appFormPool[formId] || (() =>
         <div>
-            {console.error('Undefined AppForm', formId)}
-            Undefined AppForm
+            Undefined AppForm: {formId}
         </div>)
 
-    console.log('render')
+    //console.log('render')
     return (
         <div>
             <h1>MainFrame JSX</h1>
@@ -35,6 +34,7 @@ function App() {
                 <li onClick={() => setFormId('FormA01')}>FormA01</li>
                 <li onClick={() => setFormId('FormB02')}>FormB02</li>
                 <li onClick={() => setFormId('FormC03')}>FormC03</li>
+                <li onClick={() => setFormId('FormD04')}>FormD04</li>
             </ul>
 
             <Suspense fallback={<div>Loading...</div>}>
---
+++ React/src/js/AppForm/FormD04/AppForm.jsx
@@ -0,0 +1,79 @@
+﻿import React from 'react'
+
+// 常數
+const APP_FORM_TITLE = 'FormD04'
+
+const dataList = [
+    { line: 'A', date: '1/1', field1: 'a', field2: 'a2' },
+    { line: 'B', date: '1/1', field1: 'b', field2: 'b2' },
+    { line: 'C', date: '1/1', field1: 'c', field2: 'c2' },
+    { line: 'A', date: '1/2', field1: 'a', field2: 'a2' },
+    { line: 'B', date: '1/2', field1: 'b', field2: 'b2' },
+    //{ line:'C', date:'1/2', field1:'c', field2:'c2' },
+    //{ line:'A', date:'1/3', field1:'a', field2:'a2' },
+    { line: 'B', date: '1/3', field1: 'b', field2: 'b2' },
+    { line: 'C', date: '1/3', field1: 'c', field2: 'c2' },
+]
+
+const pivot = {}
+const lineKeys = []
+const dateKeys = []
+
+dataList.forEach(item => {
+    const { line, date } = item
+
+    if (lineKeys.findIndex(c => c == line) < 0)
+        lineKeys.push(line)
+
+    if (dateKeys.findIndex(c => c == date) < 0)
+        dateKeys.push(date)
+
+    if (!(pivot[line])) pivot[line] = {}
+
+    pivot[line][date] = {
+        field1: item.field1,
+        field2: item.field2
+    };
+})
+
+
+export default function AppForm() {
+    let k = 1;
+    return (
+        <div>
+            <h2>{APP_FORM_TITLE}</h2>
+            {/*
+            <p>{JSON.stringify(lineKeys)}</p>
+            <p>{JSON.stringify(dateKeys)}</p>
+            {Object.keys(pivot).map(line =>
+                Object.keys(pivot[line]).map(date =>
+                    <p key={k++}>pivot[{line}][{date}] → {JSON.stringify(pivot[line][date])}</p>
+                )
+            )}
+            */}
+
+            <table style={{ border: '3px #cccccc solid' }} border={'1'}>
+                <thead>
+                    <tr>
+                        <th></th>
+                        {dateKeys.map((date, j) =>
+                            <th key={j}>{date}</th>
+                        )}
+                    </tr>
+                </thead>
+                <tbody>
+                    {lineKeys.map((line, i) =>
+                        <tr key={i}>
+                            <td>{line}</td>
+                            {dateKeys.map((date, j) =>
+                                <td key={j}>
+                                    {JSON.stringify(pivot[line][date])}
+                                </td>
+                            )}
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        </div>
+    )
+}
